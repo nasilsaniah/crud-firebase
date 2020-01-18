00e482eb1cf0333e36934c762dc7289ed4f4a5e7
BIB-USR-AQP265 <BIB-USR-AQP265@grupoutp.net>
2020-01-18 17:17:26 -0500

    leer datos y guardar

--- app.js
+++
@@ -1,9 +0,0 @@
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-    apiKey: "AIzaSyC8BvA9e34O80Oab_UMn873-f3ENaRAJVk",
-    authDomain:  "primerproyecto-9206f.firebaseapp.com",
-    projectId: "primerproyecto-9206f",
-});
-
-var db = firebase.firestore();
-
