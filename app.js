0d640d5bc0f9f426cd6b75415e288d7e60591c7e
BIB-USR-AQP265 <BIB-USR-AQP265@grupoutp.net>
2020-01-18 17:04:03 -0500

    inicializando firebase

---
+++ app.js
@@ -0,0 +1,9 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyC8BvA9e34O80Oab_UMn873-f3ENaRAJVk",
+    authDomain:  "primerproyecto-9206f.firebaseapp.com",
+    projectId: "primerproyecto-9206f",
+});
+
+var db = firebase.firestore();
+
