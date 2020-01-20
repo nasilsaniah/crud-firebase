0fbbfdc9cf5db44a093ca3138f53bc96cd01ce7d
LAB-USR-AQ265-A0404 <LAB-USR-AQ265-A0404@grupoutp.net>
2020-01-20 17:13:42 -0600

    editar datos-terminado

--- src/app.js
+++ src/app.js
@@ -20,15 +20,15 @@ function guardar() {
         name: nombre,
         career: carrera,
     })
-    .then(function (docRef) {
+        .then(function (docRef) {
             console.log("Document written with ID: ", docRef.id);
             //reiniciar los inputs. Una vez que se guarda, lo limpie.
             document.getElementById('nombre').value = '';
-            document.getElementById('carrera').value = ''
-    })
-    .catch(function (error) {
+            document.getElementById('carrera').value = '';
+        })
+        .catch(function (error) {
             console.error("Error adding document: ", error);
-    });
+        });
 }
 
 //leer datos
@@ -40,24 +40,60 @@ db.collection("users").onSnapshot((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data().name}`);
         tabla.innerHTML +=
-        `
+            `
         <tr>
         <th scope="row">${doc.id}</th>
             <td>${doc.data().name}</td>
             <td>${doc.data().career}</td>
             <td><button class="btn btn-danger" onclick="eliminar('${doc.id}')">Eliminar</button></td>
-            <td><button class="btn btn-warning">Editar</button></td>
+            <td><button class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().name}','${doc.data().career}')">Editar</button></td>
         </tr>
         `
     });
 });
 
 //borrar documentos
-function eliminar(id){
+function eliminar(id) {
     db.collection("users").doc(id).delete().then(function () {
         console.log("Document successfully deleted!");
     }).catch(function (error) {
         console.error("Error removing documnent: ", error);
-    });  
+    });
 }
+
+//editar documentos
+//se le pasa el id de cada documento para que le reconozca
+//variables id,nombre,carrera donde se almacenara el id,name, carrer
+
+function editar(id,nombre,carrera){
+//pasar los id del html de cada input los que estan en ' '
+    document.getElementById('nombre').value = nombre;
+    document.getElementById('carrera').value = carrera;
+    //cambiar el boton guardar por editar
+    var boton = document.getElementById('boton');
+    boton.innerHTML ='Editar';
+    //despues de hacer click en editar se ejecuta ls sgt funcion
+    
+    boton.onclick = function(){
+        var nombre = document.getElementById('nombre').value;
+        var carrera = document.getElementById('carrera').value;
+        
+        var actualizar = db.collection("users").doc(id);
+        
+        return actualizar.update({
+            name: nombre,
+            career: carrera,
+        })
+            .then(function () {
+                console.log("Document successfully updated!");
+                boton.innerHTML ='Guardar';
+                document.getElementById('nombre').value = '';
+            document.getElementById('carrera').value = '';
+            })
+            .catch(function (error) {
+                // The document probably doesn't exist.
+                console.error("Error updating document: ", error);
+            });
+    }
+    }
 
